caf0f27604588b334243e2f5f96d45f66bffe954
Denis St <haosmark@gmail.com>
2017-03-12 16:31:29 -0400

    Implement the Project class

--- ToDoTogetherAppService/DataObjects/Project.cs
+++ ToDoTogetherAppService/DataObjects/Project.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Azure.Mobile.Server;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,5 +9,13 @@ namespace ToDoTogetherAppService.DataObjects
 {
     public class Project : EntityData
     {
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+
+        [ForeignKey("OwnerId")]
+        public User Owner { get; set; }
+
+        public ICollection<TaskItem> Tasks { get; set; }
+        public ICollection<User> Collaborators { get; set; }
     }
 }
