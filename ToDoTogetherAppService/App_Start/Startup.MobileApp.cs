eedb71118174b5db335439a7bc73a5fc1f087cc8
Denis St <haosmark@gmail.com>
2017-03-12 16:47:59 -0400

    Comment out boiler plate code

--- ToDoTogetherAppService/App_Start/Startup.MobileApp.cs
+++ ToDoTogetherAppService/App_Start/Startup.MobileApp.cs
@@ -53,18 +53,18 @@ namespace ToDoTogetherAppService
     {
         protected override void Seed(ToDoTogetherAppContext context)
         {
-            List<TodoItem> todoItems = new List<TodoItem>
-            {
-                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
-                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
-            };
+            //List<TodoItem> todoItems = new List<TodoItem>
+            //{
+            //    new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+            //    new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
+            //};
 
-            foreach (TodoItem todoItem in todoItems)
-            {
-                context.Set<TodoItem>().Add(todoItem);
-            }
+            //foreach (TodoItem todoItem in todoItems)
+            //{
+            //    context.Set<TodoItem>().Add(todoItem);
+            //}
 
-            base.Seed(context);
+            //base.Seed(context);
         }
     }
 }
